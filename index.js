d8cf5c04e73f8d707b1e3a3488c744d1347937ce
Sumit jha <91518528+sumit1921184@users.noreply.github.com>
2024-02-23 11:48:08 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const sentences = document.querySelectorAll(".text-3");
+let index=0;
+
+function showNextSentence(){
+    sentences[index].style.display="none";
+    index=(index+1)%sentences.length;
+    sentences[index].style.display = "inline-block";
+}
+setInterval(showNextSentence, 1000);
+
+ let sidemenu=document.getElementById("sidemenu");
+
+function openmenu(){
+  console.log(1);
+    sidemenu.style.right="0";
+}
+function closemenu(){
+  console.log(1);
+    sidemenu.style.right="-200px";
+}
+
+
+document.getElementById('resume-link-1').addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/1lVov3SHwOxz_1cVkGUQ6RHCuJkRYeV_9/view?usp=sharing","_blank");
+  })
+  
+  document.getElementById('resume-link-2').addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/1lVov3SHwOxz_1cVkGUQ6RHCuJkRYeV_9/view?usp=sharing","_blank");
+  })
+
+
+
+
